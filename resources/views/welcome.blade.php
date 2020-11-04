e3aa69b62eda5caeb30ce90dbdaaf5cd9abb2bf0
Darwin Gomez <darwintnt@hotmail.com>
2020-11-04 15:26:02 -0500

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <h1 class="title">Refinancia API</h1>
+                    <h1 class="title">JSON:API</h1>
                 </div>
             </div>
         </div>
